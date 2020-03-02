c81026b50c27f00eb681ad5fbbd9d4ae392663a6
GinKenta <ginkenta@gmail.com>
2020-03-02 12:41:00 +0200

    feat: solution reverse int has been added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let arr = [];
+  const nArr = Math.abs(n).toString().split('');
+  for(let i = nArr.length; i >= 0; i--){
+    arr.push(nArr[i]);
+  }
+  return Number(arr.join(''));
 }
